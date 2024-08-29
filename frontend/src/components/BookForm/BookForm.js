67acf39a09b638ce7c37a6e847f088ad91fe1023
Rita Ritina <ritinarita559@gmail.com>
2024-08-29 19:58:25 +0300

    Move isLoadingViaAPI to Redux

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { FaSpinner } from 'react-icons/fa';
-import { addBook, fetchBook } from '../../redux/slices/booksSlice';
+import { addBook, fetchBook, selectIsLoadingViaAPI } from '../../redux/slices/booksSlice';
 import createBookWidthId from '../../utils/createBookWidthId';
 import booksData from '../../data/books.json';
 import './BookForm.css';
@@ -10,7 +10,7 @@ import { setError } from '../../redux/slices/errorSlice';
 const BookForm = () => {
    const [title, setTitle] = useState('');
    const [author, setAuthor] = useState('');
-   const [isLoading, setIsLoading] = useState(false);
+	const isLoading = useSelector(selectIsLoadingViaAPI)
    const dispatch = useDispatch();
 
    const handleSubmit = (e) => {
@@ -30,13 +30,8 @@ const BookForm = () => {
       dispatch(addBook(createBookWidthId(randomBook, 'manual')));
    };
 
-   const handleAddRandomViaAPI = async () => {
-      try {
-			setIsLoading(true)
-         await dispatch(fetchBook('http://localhost:4000/random-book-delayed'));
-      } finally {
-			setIsLoading(false)
-		}
+   const handleAddRandomViaAPI = () => {
+      dispatch(fetchBook('http://localhost:4000/random-book-delayed'));
    };
 
    return (
--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 import { setError } from './errorSlice';
 import createBookWidthId from '../../utils/createBookWidthId';
 
-const initialState = [];
+const initialState = {
+   books: [],
+   isLoadingViaAPI: false,
+};
 
 export const fetchBook = createAsyncThunk(
    'books/fetchBook',
@@ -13,7 +16,7 @@ export const fetchBook = createAsyncThunk(
          return res.data;
       } catch (error) {
          thunkAPI.dispatch(setError(error.message));
-         throw error;
+         return thunkAPI.rejectWithValue(error)
       }
    }
 );
@@ -23,37 +26,55 @@ const booksSlice = createSlice({
    initialState,
    reducers: {
       addBook: (state, author) => {
-         state.push(author.payload);
+         state.books.push(author.payload);
       },
       deleteBook: (state, action) => {
-         return state.filter((book) => book.id !== action.payload);
+         return {
+            ...state,
+            books: state.books.filter((book) => book.id !== action.payload),
+         };
       },
       toggleFavorite: (state, action) => {
-         return state.map((book) =>
-            book.id === action.payload
-               ? { ...book, isFavorite: !book.isFavorite }
-               : book
-         );
+         state.books.forEach((book) => {
+            if (book.id === action.payload) {
+               book.isFavorite = !book.isFavorite;
+            }
+         });
       },
    },
-	// extraReducers: {
-	// 	[fetchBook.fulfilled]: (state, action) => {
-   //       if (action.payload.title && action.payload.author) {
-   //          state.push(createBookWidthId(action.payload, 'API'));
-   //       }
-   //    }
-	// }
-   extraReducers: (builder) => {
-      builder.addCase(fetchBook.fulfilled, (state, action) => {
+   extraReducers: {
+      [fetchBook.pending]: (state) => {
+         state.isLoadingViaAPI = true;
+      },
+      [fetchBook.fulfilled]: (state, action) => {
+         state.isLoadingViaAPI = false;
          if (action.payload.title && action.payload.author) {
-            state.push(createBookWidthId(action.payload, 'API'));
+            state.books.push(createBookWidthId(action.payload, 'API'));
          }
-      });
+      },
+      [fetchBook.rejected]: (state) => {
+         state.isLoadingViaAPI = false;
+      },
    },
+   // extraReducers: (builder) => {
+   //    builder.addCase(fetchBook.pending, (state) => {
+   //       state.isLoadingViaAPI = true;
+   //    });
+   //    builder.addCase(fetchBook.fulfilled, (state, action) => {
+   //       state.isLoadingViaAPI = false;
+   //       if (action.payload.title && action.payload.author) {
+   //          state.books.push(createBookWidthId(action.payload, 'API'));
+   //       }
+   //    });
+   //    builder.addCase(fetchBook.rejected, (state) => {
+   //       state.isLoadingViaAPI = false;
+   //    });
+   // },
 });
 
 export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions;
 
-export const selectBooks = (state) => state.books;
+export const selectBooks = (state) => state.books.books;
+export const selectIsLoadingViaAPI = (state) => state.books.isLoadingViaAPI;
 
 export default booksSlice.reducer;
